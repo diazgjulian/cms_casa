87d60a4696ced973e96407d14ca78ca712190bcd
Julian <diazg.julian@gmail.com>
2024-04-24 20:22:31 +0200

    ajustes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
 
-
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-
-exec "$@"
+bundle exec rails db:migrate 2>/dev/null
+exec bundle exec "$@"
